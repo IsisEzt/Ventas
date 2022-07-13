25e7b4728eb3e613729db917f4f7855af7149f1c
IsisEzt <isaezt355@gamil.com>
2022-07-13 18:53:27 -0500

    captura de elementos

--- Pantallas/SRadd.js
+++ Pantallas/SRadd.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {useState} from 'react'
-import {ScrollView, Button, view, StyleSheet, Textinput, Text } from 'react-native'
+import {ScrollView, Button, View, StyleSheet, TextInput, Text } from 'react-native'
 
 const SRadd = () => {
 
@@ -10,12 +10,54 @@ const SRadd = () => {
           alignItems: 'center',
           justifyContent: 'center',
       },
+      input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+      },
     });
+
+
+    const [elementos,setelement]=useState({
+      producto:'',
+      precio:'',
+      existencia:'',
+      categoria:''
+    })
+
+    const capturar = (atrib,valor) =>{
+    setelement ({...elementos,[atrib]:valor})
+    console.log(elementos)
+      }
 
   return (
       <View style={styles.Contenedor}>
 
-       <Text>Listo</Text>
+        <TextInput
+        style={styles.input}
+        placeholder="Productos"
+        onChangeText={(value)=>capturar('productos',value)}
+        />
+
+        <TextInput
+        style={styles.input}
+        placeholder="Precio"
+        onChangeText={(value)=>capturar('precio',value)}
+        />
+
+        <TextInput
+        style={styles.input}
+        placeholder="Existencia"
+        onChangeText={(value)=>capturar('existencia',value)}
+        />
+
+        <TextInput
+        style={styles.input}
+        placeholder="Categoria"
+        onChangeText={(value)=>capturar('categoria',value)}
+        />
+
       </View>
     )
 }
